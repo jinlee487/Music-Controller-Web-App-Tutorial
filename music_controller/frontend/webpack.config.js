21aaba53735eaca040b1087029639c8396543ca2
jayjay <jinwoolee131@gmail.com>
2022-01-24 01:11:57 -0500

    updates video 5

---
+++ music_controller/frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development'; // dev mode by default
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "./static/frontend"),
+      filename: "[name].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+    ],
+  };
+}
